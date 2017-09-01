fb64b90639cc4127294954b7b3dbbe07486e75ef
Olga <ninjalab@ya.ru>
2017-09-01 23:36:33 +0300

    4

--- index.js
+++ index.js
@@ -16,56 +16,13 @@
     'yandex.com',
   ]
 
-  windows.MyForm = {
+  window.MyForm = {
     validate() {
-      var fio = $fio.value
-      var email = $email.value
-      var phone = $phone.value
-      var errorFields = []
-      var numOfPhone = phone.replace(NUM_OF_PHONE, '').split('')
-      var sumPhoneNumber = 0
-
-      const hasSpaces = (string) => /\s/.test(string)
-      const isYandexDomain = (domain) => YANDEX_DOMAINS.indexOf(domain) !== -1
-
-      function validateEmail(email) {
-        const emailParts = email.split('@')
-        const login = emailParts[0]
-        const domain = emailParts[1]
-
-        return (
-          emailParts.length === 2 &&
-          login &&
-          domain &&
-          isYandexDomain(domain) &&
-          !hasSpaces(login)
-        )
-      }
-
-      if (fio.trim().split(/\s+/).length !== 3) {
-        errorFields.push('fio')
-      }
-
-      if (!validateEmail(email)) {
-        errorFields.push('email')
-      }
-
-      if (PHONE_REGEX.test(phone.trim())) {
-        for (var i of numOfPhone) {
-          sumPhoneNumber = sumPhoneNumber + Number(numOfPhone[i])
-          if (sumPhoneNumber > 30){
-            errorFields.push('phone')
-          }
-        }
-      } else {
-        errorFields.push('phone')
-      }
-
-      return {
-        isValid: errorFields.length === 0,
-        errorFields: errorFields
-      }
-
+      return validate({
+        fio: $fio.value,
+        email: $email.value,
+        phone: $phone.value,
+      })
     },
 
 
@@ -100,10 +57,56 @@
   }
 
 
+  function validate(data) {
+    const { fio, email, phone } = data
+    var errorFields = []
+    var numOfPhone = phone.replace(NUM_OF_PHONE, '').split('')
+    var sumPhoneNumber = 0
+
+    const hasSpaces = (string) => /\s/.test(string)
+    const isYandexDomain = (domain) => YANDEX_DOMAINS.indexOf(domain) !== -1
+
+    function validateEmail(email) {
+      const emailParts = email.split('@')
+      const login = emailParts[0]
+      const domain = emailParts[1]
+
+      return (
+        emailParts.length === 2 &&
+        login &&
+        domain &&
+        isYandexDomain(domain) &&
+        !hasSpaces(login)
+      )
+    }
+
+    if (fio.trim().split(/\s+/).length !== 3) {
+      errorFields.push('fio')
+    }
+
+    if (!validateEmail(email)) {
+      errorFields.push('email')
+    }
+
+    if (PHONE_REGEX.test(phone.trim())) {
+      for (var i of numOfPhone) {
+        sumPhoneNumber = sumPhoneNumber + Number(numOfPhone[i])
+        if (sumPhoneNumber > 30){
+          errorFields.push('phone')
+        }
+      }
+    } else {
+      errorFields.push('phone')
+    }
+
+    return {
+      isValid: errorFields.length === 0,
+      errorFields: errorFields
+    }
+  }
 
 
-
-  function sumitForm() {
+  function submitForm() {
     ajaxSubmit(MyForm.getData(), response => {
       switch (response.status) {
         case 'success':
@@ -113,21 +116,30 @@
 
         case 'error':
           $result.classList.add('error')
-          $result.innerText = result.reason
+          $result.innerText = response.reason
           break
 
         case 'progress':
           $result.classList.add('progress')
-          setTimeout(sumitForm, response.timeout)
+          setTimeout(submitForm, response.timeout)
           break
       }
     })
   }
 
   function ajaxSubmit(data, callback) {
-    callback({ status: 'success' })
-    // { status: 'success' }
-    // { status: 'error', reason }
-    // { status: 'progress', timeout: 1000 }
+    setTimeout(() => {
+      if (Math.random() > 0.6) {
+        const { isValid, errorFields } = validate(data)
+
+        if (isValid) {
+          callback({ status: 'success' })
+        } else {
+          callback({ status: 'error', reason: `Invalid ${errorFields.join(', ')}` })
+        }
+      } else {
+        callback({ status: 'progress', timeout: 1000 })
+      }
+    }, Math.random() * 2000)
   }
 })()
